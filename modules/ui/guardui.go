43ac503fc26b0f20acdde390deb98c7b743d219c
Ivan Chobotar <ramirez.twiggy@gmail.com>
2020-02-28 10:32:08 +0200

    Added Guard API Added Guard UI

--- modules/ui/guardui.go
+++ modules/ui/guardui.go
@@ -12,13 +12,21 @@ func NewHTTPHandler() http.Handler {
 	r := chi.NewRouter()
 
 	r.Get("/guard", func(w http.ResponseWriter, r *http.Request) {
-		fp, _ := os.Open("../_ui/guard/index.html")
+		fp, err := os.Open("../_ui/guard/index.html")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 		io.Copy(w, fp)
 	})
 
 	r.Get("/assets/img/logo.png", func(w http.ResponseWriter, r *http.Request) {
 		// fp, _ := os.Open("../ui/guard/img/" + chi.URLParam(r, "filename"))
-		fp, _ := os.Open("../_ui/guard/img/logo.png")
+		fp, err := os.Open("../_ui/guard/img/logo.png")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 		io.Copy(w, fp)
 	})
 
